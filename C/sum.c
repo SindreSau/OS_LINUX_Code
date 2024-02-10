32d8df5439b70c83cee7fa66987d4ff738ef90f8
Sindre Sauarlia <101213882+SindreSau@users.noreply.github.com>
2024-02-10 12:14:05 +0100

    sum.c added

---
+++ C/sum.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int sum()
+{
+   int S=0,i;
+   for(i=0;i<4;i++)
+   {
+      S = S + i;
+   }
+   return(S);
+}
+
+int main()
+{
+   int Sum;
+   Sum = sum();
+    printf("Sum = %d\n",Sum);
+}
+
